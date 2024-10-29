6ada5b67f9ec788b8ebd65949bc91be6e6cd8499
imbeer <alekceykocenko@gmail.com>
2024-10-29 14:18:16 +0300

    Add first post controller for getting posts and creating new ones, not tested yet

---
+++ blog_backend/controllers/PostController.cpp
@@ -0,0 +1,74 @@
+#include "PostController.h"
+
+namespace drogon {
+template<>
+inline drogon_model::blog::Post fromRequest(const HttpRequest &req) {
+    auto json = req.getJsonObject();
+    auto postJson = (*json)["post"];
+    auto post = drogon_model::blog::Post(postJson);
+    return post;
+}
+}
+
+void PostController::newPost(
+    drogon_model::blog::Post &&newPost,
+    function<void(const HttpResponsePtr &)> &&callback)
+{
+    auto callbackPtr = make_shared<function<void(const HttpResponsePtr &)>>(std::move(callback));
+    m_postMapper.insert(
+        newPost,
+        [callbackPtr](const drogon_model::blog::Post &post) {
+            auto json = Json::Value();
+            json["post"] = post.toJson();
+            auto response = HttpResponse::newHttpJsonResponse(json);
+            response->setStatusCode(HttpStatusCode::k201Created);
+            (*callbackPtr)(response);
+        },
+        [callbackPtr](const drogon::orm::DrogonDbException &e) {
+            LOG_ERROR << e.base().what();
+            auto response = HttpResponse::newHttpResponse();
+            response->setStatusCode(HttpStatusCode::k400BadRequest);
+            (*callbackPtr)(response);
+        });
+}
+
+void PostController::getAllPosts(
+    const HttpRequestPtr& req,
+    std::function<void (const HttpResponsePtr &)> &&callback,
+    const string &authorUsername,
+    int offset, int limit)
+{
+    auto callbackPtr = make_shared<function<void(const HttpResponsePtr &)>>(std::move(callback));
+    Json::Value responseBody;
+    responseBody.resize(0);
+    Criteria postCriteria;
+
+    if (!authorUsername.empty()) {
+        const Criteria userCriteria(drogon_model::blog::User::Cols::_username, CompareOperator::EQ, authorUsername);
+        const auto users = m_userMapper.findBy(userCriteria);
+        if (users.empty()) {
+            LOG_ERROR << "No user found with username: " << authorUsername;
+            auto response = HttpResponse::newHttpResponse();
+            response->setStatusCode(HttpStatusCode::k400BadRequest);
+            (*callbackPtr)(response);
+            return;
+        }
+        const auto userId = users[0].getUserId();
+        postCriteria = Criteria(drogon_model::blog::Post::Cols::_user_id, CompareOperator::EQ, userId);
+    }
+
+    const auto posts = m_postMapper
+                           .orderBy(drogon_model::blog::Post::Cols::_time)
+                           .limit(limit)
+                           .offset(offset)
+                           .findBy(postCriteria);
+    cout << "Posts selected: " << posts.size() << endl;
+    for (const auto &post : posts) {
+        responseBody.append(post.toJson());
+    }
+    auto response = HttpResponse::newHttpJsonResponse(responseBody);
+    response->setStatusCode(HttpStatusCode::k201Created);
+    (*callbackPtr)(response);
+}
+
+
---
+++ blog_backend/controllers/PostController.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <drogon/HttpController.h>
+#include "../models/Post.h"
+#include "../models/User.h"
+
+using namespace drogon;
+using namespace std;
+using namespace drogon::orm;
+
+class PostController : public HttpController<PostController> {
+public:
+    METHOD_LIST_BEGIN
+        ADD_METHOD_TO(PostController::newPost, "/posts", drogon::Post, "LoginFilter");
+        ADD_METHOD_TO(PostController::getAllPosts, "/posts?author={}&offset={}&limit={}", Get);
+        // TODO: Login Filter
+    METHOD_LIST_END
+
+    void newPost(
+        drogon_model::blog::Post &&newPost,
+        function<void(const HttpResponsePtr &)> &&callback);
+    void getAllPosts(
+        const HttpRequestPtr& req,
+        std::function<void (const HttpResponsePtr &)> &&callback,
+        const string &author,
+        int offset, int limit);
+
+private:
+    drogon::orm::Mapper<drogon_model::blog::Post> m_postMapper = drogon::orm::Mapper<drogon_model::blog::Post>(app().getDbClient());
+    drogon::orm::Mapper<drogon_model::blog::User> m_userMapper = drogon::orm::Mapper<drogon_model::blog::User>(app().getDbClient());
+};
