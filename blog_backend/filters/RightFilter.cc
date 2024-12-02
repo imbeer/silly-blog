6fa9db2f686888ee69f422cd47732bdd62abac7e
imbeer <alekceykocenko@gmail.com>
2024-12-02 23:10:34 +0300

    Fixed rights filter. (it actually was just wrong input in test)

--- blog_backend/filters/RightFilter.cc
+++ blog_backend/filters/RightFilter.cc
@@ -12,11 +12,8 @@ void RightFilter::doFilter(
     FilterCallback &&fcb,
     FilterChainCallback &&fccb)
 {
-    cout << "doing filter" << endl;
-
     auto user = jwtService::getCurrentUserFromRequest(req);
     if (!user.has_value()) {
-        cout << "no user" << endl;
         auto res = drogon::HttpResponse::newHttpResponse();
         res->setStatusCode(drogon::k401Unauthorized);
         fcb(res);
@@ -25,18 +22,14 @@ void RightFilter::doFilter(
     auto json = req->getJsonObject();
     int postId = (*json)["post"]["post_id"].asInt();
 
-    cout << "get post" << endl << postId <<endl;
     auto post = postMapper.findByPrimaryKey(postId);
-    cout << post.getValueOfTextContent() << endl;
 
     if (user->getValueOfIsAdmin() || post.getValueOfUserId() == user->getValueOfUserId()) {
-        cout << "approved" << endl;
         fccb();
         return;
     }
 
     auto res = drogon::HttpResponse::newHttpResponse();
     res->setStatusCode(k403Forbidden);
-    cout << "not approved" << endl;
     fcb(res);
 }
