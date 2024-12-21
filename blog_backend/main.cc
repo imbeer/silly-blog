a22a412b623d4ec93f691183035a405cfd1de47e
imbeer <alekceykocenko@gmail.com>
2024-12-21 18:44:52 +0300

    Try to create docker image[1]

--- blog_backend/main.cc
+++ blog_backend/main.cc
@@ -2,7 +2,7 @@
 int main() {
 
     drogon::app()
-        .loadConfigFile("/home/tod/Projects/BLOG/blog_backend/config.json")
+        .loadConfigFile("../config.json")
         // .addListener("0.0.0.0", 8080)
         // .addDbClient(dbConfig)
         .run();
