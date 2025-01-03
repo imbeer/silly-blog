061aca7c3510479910af33ba97b4dbedb0dbe8c7
imbeer <alekceykocenko@gmail.com>
2025-01-03 18:36:56 +0300

    Add view model with live data binding for post lists

--- blog_android_app/app/src/main/java/com/example/blog_android_app/Config.kt
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/Config.kt
@@ -3,3 +3,5 @@ package com.example.blog_android_app
 const val BASE_URL = "http://38.180.101.54:8080/"
 const val TEST_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInVzZXIiOiIyMDEifQ.sIZdiqdm9eKzBaElBEbvzUEWTD8HcpZcbppMhWwggxY"
 const val TEST_USERNAME = "mcgeechristopher"
+const val DEFAULT_POSTS_LIMIT = 10
+const val DEFAULT_COMMENT_LIMIT = 10
--- blog_android_app/app/src/main/java/com/example/blog_android_app/feed/FeedFragment.kt
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/feed/FeedFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.blog_android_app.R
+import com.example.blog_android_app.viewmodel.PostListViewModel
 
 class FeedFragment : Fragment() {
 
@@ -21,23 +22,28 @@ class FeedFragment : Fragment() {
         val view = inflater.inflate(R.layout.feed_fragment, container, false)
         recyclerView = view.findViewById(R.id.recyclerView)
         recyclerView.layoutManager = LinearLayoutManager(context)
+        val viewModel = PostListViewModel()
 
-        adapter = PostFeedAdapter(mutableListOf())
-        adapter.loadData()
+        adapter = PostFeedAdapter(viewModel, this.viewLifecycleOwner)
+        viewModel.loadData()
         recyclerView.adapter = adapter
 
         recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
+
+                if (viewModel.isEnded() || viewModel.isLoading()) {
+                    return
+                }
+
                 val layoutManager = recyclerView.layoutManager as LinearLayoutManager
                 val visibleItemCount = layoutManager.childCount
                 val totalItemCount = layoutManager.itemCount
                 val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
 
-                if (!adapter.isEnded() && !adapter.isLoading() && (visibleItemCount + firstVisibleItemPosition) >= totalItemCount
-                    && firstVisibleItemPosition >= 0
-                ) {
-                    adapter.loadData()
+                if ((visibleItemCount + firstVisibleItemPosition) >= totalItemCount && firstVisibleItemPosition >= 0) {
+                    viewModel.loadData()
+//                    adapter.notifyItemRangeInserted()
                 }
             }
         })
