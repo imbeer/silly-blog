6ad8f7391f49987375d92441cd88c16d6c2f8c60
imbeer <alekceykocenko@gmail.com>
2025-01-05 13:15:40 +0300

    Change test name

--- blog_android_app/app/src/main/java/com/example/blog_android_app/Config.kt
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/Config.kt
@@ -4,7 +4,7 @@ import okhttp3.MediaType
 
 const val BASE_URL = "http://38.180.101.54:8080/"
 const val TEST_JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInVzZXIiOiIyMDEifQ.sIZdiqdm9eKzBaElBEbvzUEWTD8HcpZcbppMhWwggxY"
-const val TEST_USERNAME = "mcgeechristopher"
+const val TEST_USERNAME = "TheRealAmogus"
 const val DEFAULT_POSTS_LOAD_LIMIT = 10
 const val DEFAULT_POSTS_WHEN_TO_LOAD = 3
 const val DEFAULT_COMMENT_LIMIT = 10
