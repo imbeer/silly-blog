8fdf60f46a7b108af0300dfbcfaef59275c8d3fe
imbeer <alekceykocenko@gmail.com>
2025-01-05 15:52:41 +0300

    Add comment model and repository

---
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/repository/comments/CommentRestController.kt
@@ -0,0 +1,63 @@
+package com.example.blog_android_app.repository.comments
+
+import com.example.blog_android_app.JSON_TYPE
+import com.example.blog_android_app.TEST_JWT
+import com.example.blog_android_app.model.CommentData
+import com.example.blog_android_app.repository.connection.RetrofitInstance
+import okhttp3.RequestBody
+
+object CommentRestController {
+    private val api: CommentApiService
+
+    init {
+        val retrofit = RetrofitInstance.instance
+        api = retrofit.create(CommentApiService::class.java)
+    }
+
+    suspend fun fetchComments(
+        token: String = TEST_JWT,
+        postId: Int,
+        author: String = "",
+        offset: Int,
+        limit: Int
+    ): List<CommentData>? {
+        return api.getComments("Bearer $token", postId, author, offset, limit).body()
+    }
+
+    suspend fun createComment(
+        token: String = TEST_JWT,
+        commentData: CommentData
+    ): CommentData? {
+        val resp = api.createComment("Bearer $token", constructJson(commentData))
+        return if (resp.isSuccessful) {
+            resp.body()
+        } else {
+            null
+        }
+    }
+
+    suspend fun editComment(
+        token: String = TEST_JWT,
+        commentData: CommentData
+    ): CommentData? {
+        val resp = api.editComment("Bearer $token", constructJson(commentData))
+        return if (resp.isSuccessful) {
+            resp.body()
+        } else {
+            null
+        }
+    }
+
+    suspend fun deleteComment(
+        token: String = TEST_JWT,
+        commentData: CommentData
+    ): Boolean {
+        val resp = api.deleteComment("Bearer $token", constructJson(commentData))
+        return (resp.code() == 204)
+    }
+
+    private fun constructJson(commentData: CommentData): RequestBody = RequestBody.create(
+        JSON_TYPE,
+        """{"comment": {${commentData.commentId?.let { "\"comment_id\":$it," } ?: ""}"text_content":"${commentData.textContent}", "post_id":${commentData.postId}}}"""
+    )
+}
