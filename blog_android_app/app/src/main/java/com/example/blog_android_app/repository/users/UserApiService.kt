80d967d4c6c456461bf63f6a10efe6f216b2aa0e
imbeer <alekceykocenko@gmail.com>
2025-01-06 19:39:17 +0300

    Add getting user on init

--- blog_android_app/app/src/main/java/com/example/blog_android_app/repository/users/UserApiService.kt
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/repository/users/UserApiService.kt
@@ -1,10 +1,13 @@
 package com.example.blog_android_app.repository.users
 
 import com.example.blog_android_app.model.UserAndToken
+import com.example.blog_android_app.model.UserData
 import okhttp3.RequestBody
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface UserApiService {
 
@@ -17,4 +20,9 @@ interface UserApiService {
     suspend fun login(
         @Body json: RequestBody
     ): Response<UserAndToken>
+
+    @GET("/user")
+    suspend fun getUser(
+        @Query("id") id: Int
+    ): Response<UserData>
 }
