604b63b3f6eec4f848b7954d8dd78a495b1d7fd3
imbeer <alekceykocenko@gmail.com>
2025-01-07 20:59:57 +0300

    Add image view on post cards and in post fragment

--- blog_android_app/app/src/main/java/com/example/blog_android_app/view/feed/ImagePagerAdapter.kt
+++ blog_android_app/app/src/main/java/com/example/blog_android_app/view/feed/ImagePagerAdapter.kt
@@ -1,29 +1,29 @@
 package com.example.blog_android_app.view.feed
 
-import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.blog_android_app.R
+import com.example.blog_android_app.repository.images.ImageRestController
 
-class ImagePagerAdapter(private val bitmapList: MutableList<Bitmap?>) :
-    RecyclerView.Adapter<ImagePagerAdapter.BitmapViewHolder>() {
+class ImagePagerAdapter(private val imageIds: List<Int>) :
+    RecyclerView.Adapter<ImagePagerAdapter.ImageViewHolder>() {
 
-    class BitmapViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val imageView: ImageView = itemView.findViewById(R.id.image_view)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BitmapViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_image, parent, false)
-        return BitmapViewHolder(view)
+        return ImageViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: BitmapViewHolder, position: Int) {
-        holder.imageView.setImageBitmap(bitmapList[position])
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        ImageRestController.downloadImage(holder.imageView, imageIds[position])
     }
 
-    override fun getItemCount(): Int = bitmapList.size
+    override fun getItemCount(): Int = imageIds.size
 }
